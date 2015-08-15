a955267fcf481cfeba3166a3f7c2b33861371f18
MyWayToJavaJunior <hyperjava@mail.ru>
2015-08-16 03:00:33 +0600

    minor changes

--- fileworks/src/main/java/kz/e16training/fileworks/Run.java
+++ fileworks/src/main/java/kz/e16training/fileworks/Run.java
@@ -10,19 +10,6 @@ import kz.e16training.fileworks.controller.Controller;
  */
 public class Run {
     public static void main(String[] args) {
-        /*FileWork fileWork = new FileWork();
-        try {
-            System.out.println(fileWork.view("fileworks"));
-            System.out.println(fileWork.view("text"));
-            System.out.println(fileWork.view("text1"));
-            fileWork.usingFile("text31", "line1");
-            System.out.println(fileWork.view("text31"));
-            fileWork.usingFile("text31", "line2");
-            System.out.println(fileWork.view("text31"));
-            fileWork.removeFile("text31");
-        } catch (ProjectMainException e) {
-            System.out.println(e.getMessage());
-        }*/
         Controller controller = new Controller();
         controller.commandLine();
     }

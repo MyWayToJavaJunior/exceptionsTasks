7ed547eb80a9062360c550f22c5fc4e5639799f5
MyWayToJavaJunior <hyperjava@mail.ru>
2015-08-16 02:40:45 +0600

    fileworks task done

--- fileworks/src/main/java/kz/e16training/fileworks/Run.java
+++ fileworks/src/main/java/kz/e16training/fileworks/Run.java
@@ -1,6 +1,7 @@
 package kz.e16training.fileworks;
 
 
+import kz.e16training.fileworks.controller.Controller;
 
 /**
  * @author DK
@@ -9,6 +10,20 @@ package kz.e16training.fileworks;
  */
 public class Run {
     public static void main(String[] args) {
-
+        /*FileWork fileWork = new FileWork();
+        try {
+            System.out.println(fileWork.view("fileworks"));
+            System.out.println(fileWork.view("text"));
+            System.out.println(fileWork.view("text1"));
+            fileWork.usingFile("text31", "line1");
+            System.out.println(fileWork.view("text31"));
+            fileWork.usingFile("text31", "line2");
+            System.out.println(fileWork.view("text31"));
+            fileWork.removeFile("text31");
+        } catch (ProjectMainException e) {
+            System.out.println(e.getMessage());
+        }*/
+        Controller controller = new Controller();
+        controller.commandLine();
     }
 }
